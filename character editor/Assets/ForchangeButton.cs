d7addaa759f5d54c69fca869378515048728ebaa
김연우 <rladusdn56@gmail.com>
2023-10-04 03:51:04 +0900

    code change with chatai

--- character editor/Assets/ForchangeButton.cs
+++ character editor/Assets/ForchangeButton.cs
@@ -5,30 +5,31 @@ using UnityEngine;
 public class ForchangeButton : MonoBehaviour
 {
     public GameObject[] hairs;
-    public bool[] hasHairs;
+    public Hairchanger hairchangerComponent; // Hairchanger 컴포넌트를 직접 참조할 변수
     bool sDown1;
     bool sDown2;
     bool sDown3;
-    Hairchanger hairchangerComponent; // Hairchanger 컴포넌트를 저장할 변수
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Hairchanger 컴포넌트를 가지고 있는 게임 오브젝트를 찾아서 가져옴
+        // Hairchanger 컴포넌트를 이 스크립트에서 직접 참조
         hairchangerComponent = FindObjectOfType<Hairchanger>();
+        if (hairchangerComponent == null)
+        {
+            Debug.LogError("Hairchanger 컴포넌트를 찾을 수 없습니다.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        GetInput(); // 숫자 키 입력을 감지
+        GetInput();
         
-        if (Input.GetKeyDown(KeyCode.Alpha1)) // "1" 키 입력을 감지
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Debug.Log("1을 눌렀습니다.");
         }
         
-        Swap(); // Swap 함수 호출
+        Swap();
     }
 
     void Swap()
@@ -38,7 +39,7 @@ public class ForchangeButton : MonoBehaviour
         if (sDown2) hairsIndex = 1;
         if (sDown3) hairsIndex = 2;
 
-        if (hairchangerComponent != null)
+        if (hairsIndex != -1 && hairchangerComponent != null)
         {
             // Hairchanger 스크립트에서 정의한 type과 value를 가져와서 사용
             Hairchanger.Type type = hairchangerComponent.type;
@@ -52,7 +53,7 @@ public class ForchangeButton : MonoBehaviour
                 }
             }
 
-            if (hairsIndex != -1)
+            if (hairsIndex >= 0 && hairsIndex < hairs.Length)
             {
                 hairs[hairsIndex].SetActive(true); // 선택한 asset 활성화
             }
@@ -61,16 +62,12 @@ public class ForchangeButton : MonoBehaviour
             Debug.Log("Type: " + type);
             Debug.Log("Value: " + value);
         }
-        else
-        {
-            Debug.LogWarning("Hairchanger 컴포넌트를 찾을 수 없습니다.");
-        }
     }
 
     void GetInput()
     {
-        sDown1 = Input.GetKeyDown(KeyCode.Alpha1); // 숫자 1 키 입력 감지
-        sDown2 = Input.GetKeyDown(KeyCode.Alpha2); // 숫자 2 키 입력 감지
-        sDown3 = Input.GetKeyDown(KeyCode.Alpha3); // 숫자 3 키 입력 감지
+        sDown1 = Input.GetKeyDown(KeyCode.Alpha1);
+        sDown2 = Input.GetKeyDown(KeyCode.Alpha2);
+        sDown3 = Input.GetKeyDown(KeyCode.Alpha3);
     }
 }
